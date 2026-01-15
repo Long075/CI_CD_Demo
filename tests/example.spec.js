f34dca711ebd010d2be15d1ef6a82655d8f9e04e
Lango <longxinhxan75@gmail.com>
2026-01-15 11:51:11 +0700

    Chạy thử CI CD lần 2

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,10 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 const token = process.env.API_TOKEN;
+const baseURL = process.env.BASE_URL;
 console.log(token);
 
 test('API GET Request', async ({request}) => {
-    const response = await request.get(`http://118.70.81.203:8010/api/users/C1442FA0-99D3-11F0-8D36-198242D8A96B`, {
+    const response = await request.get(`${baseURL}/C1442FA0-99D3-11F0-8D36-198242D8A96B`, {
         headers: {Authorization: `Bearer ${token}`}
     });
     expect(response.status()).toBe(200);
